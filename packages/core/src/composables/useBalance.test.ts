cd06a72c22f1ec7c99ecd5a05219b4a53f7901ce
Luka Saric <luka.saric@scayle.com>
2024-03-19 14:24:18 +0100

    fix: remove non-existing composables

--- packages/core/src/composables/useBalance.test.ts
+++ packages/core/src/composables/useBalance.test.ts
@@ -1,48 +1,48 @@
-import { describe, expect, it } from 'vitest'
-import { accounts } from '../../test/devnet'
-import { renderHook, waitFor } from '../../test/react'
+// import { describe, expect, it } from 'vitest'
+// import { accounts } from '../../test/devnet'
+// import { renderHook, waitFor } from '../../test/react'
 
-import { useBalance } from './useBalance'
+// import { useBalance } from './useBalance'
 
-describe('useBalance', () => {
-  describe('when address is undefined', () => {
-    it('returns no balance', async () => {
-      const { result } = renderHook(() => useBalance({}))
+// describe('useBalance', () => {
+//   describe('when address is undefined', () => {
+//     it('returns no balance', async () => {
+//       const { result } = renderHook(() => useBalance({}))
 
-      await waitFor(() => {
-        expect(result.current.status).toEqual('error')
-      })
+//       await waitFor(() => {
+//         expect(result.current.status).toEqual('error')
+//       })
 
-      expect(result.current).toMatchInlineSnapshot(`
-        {
-          "data": undefined,
-          "error": [Error: address is required],
-          "fetchStatus": "idle",
-          "isError": true,
-          "isFetching": false,
-          "isLoading": false,
-          "isPending": false,
-          "isSuccess": false,
-          "refetch": [Function],
-          "status": "error",
-        }
-      `)
-    })
-  })
+//       expect(result.current).toMatchInlineSnapshot(`
+//         {
+//           "data": undefined,
+//           "error": [Error: address is required],
+//           "fetchStatus": "idle",
+//           "isError": true,
+//           "isFetching": false,
+//           "isLoading": false,
+//           "isPending": false,
+//           "isSuccess": false,
+//           "refetch": [Function],
+//           "status": "error",
+//         }
+//       `)
+//     })
+//   })
 
-  describe('when address is defined', () => {
-    // Some issue with the RPC provider.
-    it.skip('returns the balance', async () => {
-      const { result } = renderHook(() =>
-        useBalance({ address: accounts.goerli[0].address }),
-      )
+//   describe('when address is defined', () => {
+//     // Some issue with the RPC provider.
+//     it.skip('returns the balance', async () => {
+//       const { result } = renderHook(() =>
+//         useBalance({ address: accounts.goerli[0].address }),
+//       )
 
-      await waitFor(() => {
-        expect(result.current.status).toEqual('success')
-      })
+//       await waitFor(() => {
+//         expect(result.current.status).toEqual('success')
+//       })
 
-      expect(result.current).toMatchInlineSnapshot(`
-      `)
-    })
-  })
-})
+//       expect(result.current).toMatchInlineSnapshot(`
+//       `)
+//     })
+//   })
+// })
